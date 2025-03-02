ee53ba1ec8ece566730f917d890ebc90c91169f9
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-03-02 15:13:40 +0530

    Update yash.js

--- app/components/yash.js
+++ app/components/yash.js
@@ -4,7 +4,7 @@ const Yash = () => {
     return (
 
         <div className='bg-gray-800 absolute h-full w-full z-[2]'>
-            <div className='flex items-center justify-center text-gray-200 absolute top-[10%] w-full px-5 text-xl'>Document</div>
+            <div className='flex items-center justify-center text-gray-200 absolute top-[10%] w-full px-5 text-xl'>Emberleaf Docs</div>
             <h1 className='absolute text-gray-400 text-[120px] top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%]'>Docs.</h1>
         </div>
 
