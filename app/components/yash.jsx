18b763ac118511f25bb9727a5a130c6480cf9a39
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-03-04 00:21:34 +0530

    Create yash.jsx

---
+++ app/components/yash.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Yash = () => {
+    return (
+        <div className='bg-gray-800 absolute h-full w-full z-[2]'>
+            <div className='flex items-center justify-center text-gray-200 absolute top-[10%] w-full px-5 text-xl md:text-2xl lg:text-3xl'>Docs</div>
+            <h1 className='absolute text-gray-400 text-[40px] md:text-[60px] lg:text-[80px] top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%]'>Emberleaf Docs.</h1>
+        </div>
+    )
+}
+
+export default Yash
